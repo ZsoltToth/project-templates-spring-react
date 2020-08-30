8a7a3e663755fd2a6ee2cedd60d3476d61c957ff
Zsolt Toth <tothzs87@gmail.com>
2020-08-30 06:50:38 +0200

    Docker start scripts enhanced.
    connect_to_docker_mysql.sh can be used to connect with MySQL CLI.
    
    docker_start_mysql.sh loads init script ( create_database.sql ) to the docker image so it will create the database automatically.

--- backend/src/main/resources/docker_start_mysql.sh
+++ backend/src/main/resources/docker_start_mysql.sh
@@ -6,4 +6,5 @@ docker run \
      -e MYSQL_ROOT_PASSWORD=$MYSQL_ROOT_PASS \
      --rm -d \
      -p 3306:3306 \
+     -v $(pwd)/sql/create_database.sql:/docker-entrypoint-initdb.d/create_database.sql \
      mysql:8
