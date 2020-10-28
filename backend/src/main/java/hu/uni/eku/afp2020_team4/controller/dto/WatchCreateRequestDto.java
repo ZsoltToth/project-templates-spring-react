dd305dd0b18a556dfd9e7a8ab8379be202d1c468
vereczkibalint <vereczkibalint@gmail.com>
2020-10-28 11:59:15 +0100

    watch dto and create request dto created

---
+++ backend/src/main/java/hu/uni/eku/afp2020_team4/controller/dto/WatchCreateRequestDto.java
@@ -0,0 +1,14 @@
+package hu.uni.eku.afp2020_team4.controller.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class WatchCreateRequestDto {
+    private String watchId;
+}
---
+++ backend/src/main/java/hu/uni/eku/afp2020_team4/controller/dto/WatchDto.java
@@ -0,0 +1,10 @@
+package hu.uni.eku.afp2020_team4.controller.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class WatchDto {
+    private String watchId;
+}
