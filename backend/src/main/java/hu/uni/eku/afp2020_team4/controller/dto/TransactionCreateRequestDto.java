22f9f7cb1a17dfc56bba4e4cf381ee2be7880013
KadamQ <kovadam95@gmail.com>
2020-11-03 20:40:36 +0100

    Create TransactionController
    Ref: #19

--- backend/src/main/java/hu/uni/eku/afp2020_team4/controller/dto/TransactionCreateRequestDto.java
+++ backend/src/main/java/hu/uni/eku/afp2020_team4/controller/dto/TransactionCreateRequestDto.java
@@ -1,12 +1,16 @@
 package hu.uni.eku.afp2020_team4.controller.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.Date;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class TransactionCreateRequestDto {
     private int userId;
     private int watchId;
