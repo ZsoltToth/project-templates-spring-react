4d8871e9522bf03dd1c2af6270bc4cbcb26dca9a
vereczkibalint <vereczkibalint@gmail.com>
2020-11-05 19:41:25 +0100

    implemented repository, created model and entity
    Ref: #29

---
+++ backend/src/main/java/hu/uni/eku/afp2020_team4/dao/WatchRepository.java
@@ -0,0 +1,7 @@
+package hu.uni.eku.afp2020_team4.dao;
+
+import hu.uni.eku.afp2020_team4.dao.entity.Watch;
+import org.springframework.data.repository.CrudRepository;
+
+public interface WatchRepository extends CrudRepository<Watch, Integer> {
+}
---
+++ backend/src/main/java/hu/uni/eku/afp2020_team4/model/Watch.java
@@ -0,0 +1,12 @@
+package hu.uni.eku.afp2020_team4.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Watch {
+    private String watchId;
+}
