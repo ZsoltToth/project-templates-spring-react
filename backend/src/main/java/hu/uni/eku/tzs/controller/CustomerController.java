c3c7f4c79ef34546482390275c14d0570577b7a2
Bence <csattos.bence@gmail.com>
2020-11-16 15:12:33 +0100

    Customer Controller created

---
+++ backend/src/main/java/hu/uni/eku/tzs/controller/CustomerController.java
@@ -0,0 +1,52 @@
+package hu.uni.eku.tzs.controller;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Collection;
+import java.util.stream.Collector;
+
+@RestController
+@RequestMapping
+@Api(tags = "Customer")
+@Slf4j
+public class CustomerController {
+    
+    private final CustomerService service;
+    
+    @PostMapping("/record")
+    @ApiOperation(value = "Record")
+    public void record(@RequestBody CustomerRecordRequestDto request){
+        log.info("Recording of customer ({},{})",request.getId(),request.getName());
+        try {
+            service.record(new Customer(request.getId(),request.getName(),
+                    request.getAddress(),request.getPhoneNumber(),request.getEmail()));
+        }catch (CustomerAlreadyExistsException exception){
+            log.info("Customer ({},{}) is already exists! Message: {}",request.getId(),request.getName(),exception.getMessage());
+            throw new ResponseStatusException(
+                    HttpStatus.CONFLICT,
+                    exception.getMessage()
+            );
+
+        }
+    }
+
+    @GetMapping(value = {"/"}, produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    @ApiOperation(value = "Query customer")
+    public Collection<Customer>query(){
+        return service.readAll().stream().map(model ->
+                CustomerDto.builder()
+                .id(model.getId())
+                .name(model.getName())
+                .address(model.getAddress())
+                .phoneNumber(model.getPhoneNumber())
+                .build()
+        ).collect(Collector.toList());
+    }
+}
