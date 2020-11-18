eec44810af61356f9752f91ca70c58f963628de2
momsz <donkomark@gmail.com>
2020-11-18 21:39:34 +0100

    Reformat

--- backend/src/test/java/hu/uni/eku/team3/ApplicationContextTest.java
+++ backend/src/test/java/hu/uni/eku/team3/ApplicationContextTest.java
@@ -1,4 +1,4 @@
-package hu.uni.eku.tzs;
+package hu.uni.eku.team3;
 
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 
--- backend/src/test/java/hu/uni/eku/team3/service/ComplexNumberServiceImplTest.java
+++ backend/src/test/java/hu/uni/eku/team3/service/ComplexNumberServiceImplTest.java
@@ -1,8 +1,8 @@
-package hu.uni.eku.tzs.service;
+package hu.uni.eku.team3.service;
 
-import hu.uni.eku.tzs.dao.ComplexNumberDao;
-import hu.uni.eku.tzs.model.ComplexNumber;
-import hu.uni.eku.tzs.service.exceptions.ComplexNumberAlreadyExistsException;
+import hu.uni.eku.team3.dao.ComplexNumberDao;
+import hu.uni.eku.team3.model.ComplexNumber;
+import hu.uni.eku.team3.service.exceptions.ComplexNumberAlreadyExistsException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
