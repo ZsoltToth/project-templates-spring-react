e36e349b673797f0a53ef732ec396ff729e523d2
Guti <g.a.a612.ag@gmail.com>
2020-11-30 07:38:24 +0100

    One word change
    BookingActionConstants->GuestActionConstant

--- frontend/src/store/GuestStore.js
+++ frontend/src/store/GuestStore.js
@@ -1,6 +1,6 @@
 import {EventEmitter} from 'events'
 import dispatcher from "../dispatcher/Dispatcher";
-import {refresh} from '../dispatcher/BookingActionConstants';
+import {refresh} from '../dispatcher/GuestActionConstants';
 
 class GuestStore extends EventEmitter{
 
